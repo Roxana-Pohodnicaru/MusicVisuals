1ba703f2ae5e78db5a769c1f70b945d55ac2369d
roxi-26 <roxypohodnicaru@gmail.com>
2023-04-17 16:52:02 +0100

    base file

---
+++ java/src/example/parts/backgrounds/Planet.java
@@ -0,0 +1,34 @@
+package example.parts.backgrounds;
+
+import processing.core.*;
+
+import example.MyVisual;
+
+
+public class Planet
+{
+    MyVisual mv;
+
+    float cy = 0;
+
+    float cx = 0;
+
+
+    public Planet(MyVisual mv)
+    {
+        this.mv = mv;
+
+        cy = this.mv.height/2;
+
+        cx = this.mv.width/2;
+    }
+
+
+    public void render()
+    {
+        mv.colorMode(PApplet.RGB);
+
+        mv.ellipse(cx, cx, cy, cx);
+        
+    }
+}
